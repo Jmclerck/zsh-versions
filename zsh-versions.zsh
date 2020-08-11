f1ca7c5f25d3188c6752f95c78bd235cb40e737d
Jon Clerck <j.m.clerck@icloud.com>
2020-08-11 09:27:38 +0100

    Fix icons showing when version is empty
    Signed-off-by: Jon Clerck <j.m.clerck@icloud.com>

--- zsh-versions.zsh
+++ zsh-versions.zsh
@@ -5,37 +5,57 @@ function versions() {
 
   if [[ $ZSH_DENO_VERSION == true ]]; then
     local version=$(deno --version | rg -o "deno (\d*\.\d*\.\d*)" -r '$1') &> /dev/null
-    icons+=("%{$reset_color%} $version")
+  
+    if [[ -n $version ]]; then
+      icons+=("%{$reset_color%} $version")
+    fi
   fi
 
   if [[ $ZSH_DOCKER_VERSION == true ]]; then
     local version=$(docker --version | rg -o "\d*\.\d*\.\d*") &> /dev/null
-    icons+=("%{$fg[cyan]%} $version%{$reset_color%}")
+    if [[ -n $version ]]; then
+      icons+=("%{$fg[cyan]%} $version%{$reset_color%}")
+    fi
   fi
 
   if [[ $ZSH_NODE_VERSION == true ]]; then
     local version=$(node --version | rg -o "\d*\.\d*\.\d*") &> /dev/null
-    icons+=("%{$fg[green]%} $version%{$reset_color%}")
+  
+    if [[ -n $version ]]; then
+      icons+=("%{$fg[green]%} $version%{$reset_color%}")
+    fi
   fi
 
   if [[ $ZSH_NPM_VERSION == true ]]; then
     local version=$(npm --version) &> /dev/null
-    icons+=("%{$fg[red]%} $version%{$reset_color%}")
+    
+    if [[ -n $version ]]; then
+      icons+=("%{$fg[red]%} $version%{$reset_color%}")
+    fi
   fi
 
   if [[ $ZSH_PYTHON_VERSION == true ]]; then
     local version=$(python3 --version | rg -o "\d*\.\d*\.\d*") &> /dev/null
-    icons+=("%{$fg[yellow]%} $version%{$reset_color%}")
-   fi
+
+    if [[ -n $version ]]; then
+      icons+=("%{$fg[yellow]%} $version%{$reset_color%}")
+     fi
+  fi
 
   if [[ $ZSH_RUBY_VERSION == true ]]; then
     local version=$(ruby --version | rg -o "\d*\.\d*\.\d*") &> /dev/null
-    icons+=("%{$fg[red]%} $version%{$reset_color%}")
+    
+    if [[ -n $version ]]; then
+      icons+=("%{$fg[red]%} $version%{$reset_color%}")
+    fi
   fi
 
   if [[ $ZSH_YARN_VERSION == true ]]; then
     local version=$(yarn --version) &> /dev/null
-    icons+=("%{$fg[cyan]%} $version%{$reset_color%}")
+
+    if [[ -n $version ]]; then
+      icons+=("%{$fg[cyan]%} $version%{$reset_color%}")
+    fi
   fi
 
   print $icons
