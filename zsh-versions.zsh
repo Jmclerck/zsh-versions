835ed02adc6e4fc60ae9524e07fd7490f417ca13
Jonathan Clerck <j.m.clerck@icloud.com>
2021-10-22 17:04:40 +0100

    Replace ripgrep

--- zsh-versions.zsh
+++ zsh-versions.zsh
@@ -3,24 +3,9 @@ autoload -U colors && colors
 function versions() {
   local icons=()
 
-  if [[ $ZSH_DENO_VERSION == true ]]; then
-    local version=$(deno --version | rg -o "deno (\d*\.\d*\.\d*)" -r '$1') &> /dev/null
-  
-    if [[ -n $version ]]; then
-      icons+=("%{$reset_color%} $version")
-    fi
-  fi
-
-  if [[ $ZSH_DOCKER_VERSION == true ]]; then
-    local version=$(docker --version | rg -o "\d*\.\d*\.\d*") &> /dev/null
-   
-    if [[ -n $version ]]; then
-      icons+=("%{$fg[cyan]%} $version%{$reset_color%}")
-    fi
-  fi
 
   if [[ $ZSH_NODE_VERSION == true ]]; then
-    local version=$(node --version | rg -o "\d*\.\d*\.\d*") &> /dev/null
+    local version=$(node --version | grep -o "\d*\.\d*\.\d*") &> /dev/null
   
     if [[ -n $version ]]; then
       icons+=("%{$fg[green]%} $version%{$reset_color%}")
@@ -32,22 +17,6 @@ function versions() {
     
     if [[ -n $version ]]; then
       icons+=("%{$fg[red]%} $version%{$reset_color%}")
-    fi
-  fi
-
-  if [[ $ZSH_PYTHON_VERSION == true ]]; then
-    local version=$(python3 --version | rg -o "\d*\.\d*\.\d*") &> /dev/null
-
-    if [[ -n $version ]]; then
-      icons+=("%{$fg[yellow]%} $version%{$reset_color%}")
-     fi
-  fi
-
-  if [[ $ZSH_RUBY_VERSION == true ]]; then
-    local version=$(ruby --version | rg -o "\d*\.\d*\.\d*") &> /dev/null
-    
-    if [[ -n $version ]]; then
-      icons+=("%{$fg[red]%} $version%{$reset_color%}")
     fi
   fi
 
